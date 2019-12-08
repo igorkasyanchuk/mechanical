12fa9785f1b46bda1c984a55f4d0d3b67c895489
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2019-12-08 12:56:32 -0800

    timestamp generator for migration

--- lib/generators/mechanical_generator.rb
+++ lib/generators/mechanical_generator.rb
@@ -2,6 +2,6 @@ class MechanicalGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def copy_initializer
     template 'mechanical.rb', 'config/initializers/mechanical.rb'
-    template '20190912181342_create_mechanical_mechanical_stores.rb', 'db/migrate/20190912181342_create_mechanical_mechanical_stores.rb'
+    template '20190912181342_create_mechanical_mechanical_stores.rb', "db/migrate/#{Time.now.strftime('%Y%m%d%H%M%S')}_create_mechanical_mechanical_stores.rb"
   end
 end
